388c3dca229f4e99b9389f0e2e886c98b245e035
oandeliz <oandeliz@gmail.com>
2025-02-20 13:37:35 -0500

    change the theme of app for red and added a light and dark them mode. The theme mode is according to the users them

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_list/pages/home_page.dart';
 
@@ -13,10 +14,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red,),
-            useMaterial3: true,
-      ),
-    home: HomePage(),);
+      // The Mandy red, light theme.
+      theme: FlexThemeData.light(scheme: FlexScheme.mandyRed),
+      // The Mandy red, dark theme.
+      darkTheme: FlexThemeData.dark(scheme: FlexScheme.mandyRed),
+      // Use dark or light theme based on system setting.
+      themeMode: ThemeMode.system,
+      home: HomePage(),
+    );
   }
 }
